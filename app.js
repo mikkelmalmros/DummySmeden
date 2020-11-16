a5d15a6a86193be648324e665e30edaed3e0737c
Mikkel Malmros <mikkelskovmalmros@hotmail.com>
2020-11-16 09:18:45 +0100

    Running test

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//hhehehehhehe
+// Requires
+const express = require('express')
+
+// Attributes
+let port = process.env.PORT || 8080
+const app = express()
+
+//End points
+app.get('/', (req, res) => {
+    res.send('Its running')
+})
+
+app.listen(port, () => {
+    console.log('Serveren kører');
+})
